2a73e27def4d99b1ece44e2f5321382ec2e43dc3
MayNiklas <info@niklas-steffen.de>
2023-12-19 17:07:54 +0100

    create flake setup

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
-  description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        });
+    in
+    {
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      formatter = forAllSystems
+        (system: nixpkgsFor.${system}.nixpkgs-fmt);
 
-  };
+      overlays.default = final: prev: {
+        bonn-mensa = with final;
+          pkgs.python3Packages.callPackage ./nix/pkgs/bonn-mensa { inherit self; };
+      };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.bonn-mensa;
+          bonn-mensa = pkgs.bonn-mensa;
+        }
+      );
+
+    };
 }
