9e632d31ee81f9ea00988dce7789dda937c4ac20
MayNiklas <info@niklas-steffen.de>
2023-12-20 23:16:37 +0100

    add homebrew Formula

---
+++ Formula/bonn_mensa.rb
@@ -0,0 +1,55 @@
+class BonnMensa < Formula
+  include Language::Python::Virtualenv
+
+  desc "Meal plans for university canteens in Bonn"
+  homepage "https://github.com/alexanderwallau/bonn-mensa"
+  url "https://github.com/alexanderwallau/bonn-mensa/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "80c6b84219827d16f96b81ac11c903bbc8dfba58025e115e426fd57fbf587d5b"
+  license "MIT"
+
+  head do
+    url "https://github.com/alexanderwallau/bonn-mensa.git", branch: "main"
+  end
+
+  depends_on "python"
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/d4/91/c89518dd4fe1f3a4e3f6ab7ff23cb00ef2e8c9adf99dacc618ad5e068e28/certifi-2023.11.17.tar.gz"
+    sha256 "9b469f3a900bf28dc19b8cfbf8019bf47f7fdd1a65a1d4ffb98fc14166beb4d1"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
+    sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
+  end
+
+  resource "colorama" do
+    url "https://files.pythonhosted.org/packages/d8/53/6f443c9a4a8358a93a6792e2acffb9d9d5cb0a5cfd8802644b7b1c9a02e4/colorama-0.4.6.tar.gz"
+    sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
+    sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/36/dd/a6b232f449e1bc71802a5b7950dc3675d32c6dbc2a1bd6d71f065551adb6/urllib3-2.1.0.tar.gz"
+    sha256 "df7aa8afb0148fa78488e7899b2c59b5f4ffcfa82e6c54ccb9dd37c1d7b52d54"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    # check if help text is printed
+    output = shell_output("#{bin}/mensa --help")
+    assert_match "usage:", output
+  end
+end
