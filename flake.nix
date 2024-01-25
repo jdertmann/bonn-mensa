9e9ce8edb79df06052463bc516d166c12ac25b35
MayNiklas <info@niklas-steffen.de>
2024-01-25 11:59:06 +0100

    update flake.nix Now uses input nixpkgs for overlay instead of final

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
                 (python3.withPackages (p: with p; [
                   black
                   colorama
+                  lxml
                   requests
                 ]))
               ];
@@ -32,48 +33,49 @@
           { };
       });
 
-      overlays.default = final: prev: {
-        bonn-mensa = final.callPackage
-          ({ lib, python3 }:
-
-            python3.pkgs.buildPythonApplication {
-              pname = "bonn-mensa";
-              version = (lib.strings.removePrefix ''__version__ = "'' (lib.strings.removeSuffix ''"
-              ''
-                (builtins.readFile "${self}/src/bonn_mensa/version.py")
-              ));
-              pyproject = true;
-
-              src = self;
-
-              nativeBuildInputs = with python3.pkgs; [
-                setuptools
-              ];
-
-              propagatedBuildInputs = with python3.pkgs; [
-                colorama
-                requests
-                lxml
-              ];
-
-              pythonImportsCheck = [ "bonn_mensa" ];
-
-              meta = with lib; {
-                description = "Meal plans for university canteens in Bonn";
-                homepage = "https://github.com/alexanderwallau/bonn-mensa";
-                license = licenses.mit;
-                maintainers = with maintainers; [ alexanderwallau MayNiklas ];
-                mainProgram = "mensa";
-              };
-            })
-          { };
-      };
-
       packages = forAllSystems (system:
         let pkgs = nixpkgsFor.${system}; in {
-          default = pkgs.bonn-mensa;
-          bonn-mensa = pkgs.bonn-mensa;
+          default = self.packages.${system}.bonn-mensa;
+          bonn-mensa = pkgs.callPackage
+            ({ lib, python3 }:
+
+              python3.pkgs.buildPythonApplication {
+                pname = "bonn-mensa";
+                version = (lib.strings.removePrefix ''__version__ = "'' (lib.strings.removeSuffix ''"
+              ''
+                  (builtins.readFile "${self}/src/bonn_mensa/version.py")
+                ));
+                pyproject = true;
+
+                src = self;
+
+                nativeBuildInputs = with python3.pkgs; [
+                  setuptools
+                ];
+
+                propagatedBuildInputs = with python3.pkgs; [
+                  colorama
+                  requests
+                  lxml
+                ];
+
+                pythonImportsCheck = [ "bonn_mensa" ];
+
+                meta = with lib; {
+                  description = "Meal plans for university canteens in Bonn";
+                  homepage = "https://github.com/alexanderwallau/bonn-mensa";
+                  license = licenses.mit;
+                  maintainers = with maintainers; [ alexanderwallau MayNiklas ];
+                  mainProgram = "mensa";
+                };
+              })
+            { };
         });
+
+      overlays.default = final: prev:
+        let system = "${final.system}"; in {
+          bonn-mensa = self.packages.${system}.bonn-mensa;
+        };
 
     };
 }
