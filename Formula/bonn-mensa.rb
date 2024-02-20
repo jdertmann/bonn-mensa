9ade6dd8bfccc3e4bda2af669320545c39fe2746
Niklas <44636701+MayNiklas@users.noreply.github.com>
2024-02-21 00:08:28 +0100

    Formula: update dependencies & add live check

--- Formula/bonn-mensa.rb
+++ Formula/bonn-mensa.rb
@@ -7,15 +7,14 @@ class BonnMensa < Formula
   sha256 "22c39ad3de6ae3ec9bc1dbc9363a574cc31d53947a29e07fae8527746098c776"
   license "MIT"
 
-  head do
-    url "https://github.com/alexanderwallau/bonn-mensa.git", branch: "main"
-  end
+  head "https://github.com/alexanderwallau/bonn-mensa.git", branch: "main"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
+  depends_on :macos
   depends_on "python"
 
   resource "certifi" do
@@ -33,6 +32,11 @@ class BonnMensa < Formula
     sha256 "08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44"
   end
 
+  resource "holidays" do
+    url "https://files.pythonhosted.org/packages/eb/82/8d1d8947d75bf21ec63549346eb0147e20ae4d5ef2ce9f62056f65d9b369/holidays-0.43.tar.gz"
+    sha256 "29e2c5436207dd268a3702338bcc92032aaacf47ed3bb324ff9ba8170e6d0932"
+  end
+
   resource "idna" do
     url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
     sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
@@ -43,14 +47,24 @@ class BonnMensa < Formula
     sha256 "3eea6ed6e6c918e468e693c41ef07f3c3acc310b70ddd9cc72d9ef84bc9564ca"
   end
 
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/4c/c4/13b4776ea2d76c115c1d1b84579f3764ee6d57204f6be27119f13a61d0a9/python-dateutil-2.8.2.tar.gz"
+    sha256 "0123cacc1627ae19ddf3c27a5de5bd67ee4586fbdd6440d9748f8abb483d3e86"
+  end
+
   resource "requests" do
     url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
 
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  end
+
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/e2/cc/abf6746cc90bc52df4ba730f301b89b3b844d6dc133cb89a01cfe2511eb9/urllib3-2.2.0.tar.gz"
-    sha256 "051d961ad0c62a94e50ecf1af379c3aba230c66c710493493560c0c223c49f20"
+    url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
+    sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
   end
 
   def install
